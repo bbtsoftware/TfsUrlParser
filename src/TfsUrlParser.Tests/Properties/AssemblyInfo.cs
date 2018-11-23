af505e377820c4670518f7927599a0361c2cfa04
Pascal Berger <pascal.berger@gmail.com>
2018-11-23 08:09:09 +0100

    (GH-28) Target .NET Standard 2.0

--- src/TfsUrlParser.Tests/Properties/AssemblyInfo.cs
+++ src/TfsUrlParser.Tests/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("TfsUrlParser")]
-[assembly: AssemblyDescription("Tests for Azure DevOps URL Parser URL Parser")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BBT Software AG")]
-[assembly: AssemblyProduct("TfsUrlParser")]
-[assembly: AssemblyCopyright("Copyright © BBT Software AG")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -20,16 +8,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6d16103d-8d2a-4e00-862a-2ad3c00e2883")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
